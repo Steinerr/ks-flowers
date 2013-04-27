e84a0679a0730acbc14d754ac448d1f12f2336c6
Denis Ivanov <denis19111990@gmail.com>
2013-04-28 00:57:19 +0400

    price верстка

--- templates/content.php
+++ templates/content.php
@@ -9,6 +9,7 @@
                 // Блок с картинкой
                 echo '<div class="span4 block deb">';
                 echo '<img src="static/img/flowers/fl1.jpeg" class="image">';
+                echo '<p class="price">850 <span>руб.</span></p>';
                 //Блок с информаией о картинке
                 echo '<div class="img-info"><p><a href="#">Глупое название</a></p><p>Стоимость: <s>820 р.</s>/ 500 р.</p></div>';
                 echo '</div>';
@@ -27,11 +28,12 @@
             
             for($j=1; $j<=3; $j++){
                 // Блок с картинкой
-                echo '<div class="span4 block deb">';
-                echo '<img src="static/img/flowers/fl1.jpeg" class="image">';
-                //Блок с информаией о картинке
-                echo '<div class="img-info"><p><a href="#">Глупое название</a></p><p>Стоимость: <s>820 р.</s>/ 500 р.</p></div>';
-                echo '</div>';
+                    echo '<div class="span4 block deb">';
+                        echo '<img src="static/img/flowers/fl1.jpeg" class="image">';
+                        echo '<p class="price">850 <span>руб.</span></p>';
+                        //Блок с информаией о картинке
+                        echo '<div class="img-info"><p><a href="#">Глупое название</a></p><p>Стоимость: <s>820 р.</s>/ 500 р.</p></div>';
+                    echo '</div>';
             }
             
             echo '</div>';
